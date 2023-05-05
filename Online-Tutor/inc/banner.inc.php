2dfbb596a56f60928cdfd0ff8c62a0bdaa7024e6
Md. Neehad Kadir <114414046+neehad1@users.noreply.github.com>
2023-05-05 15:56:01 +0600

    main

---
+++ Online-Tutor/inc/banner.inc.php
@@ -0,0 +1,2 @@
+<h1><a href="index.php">Online<span> Tutor</span></a></h1>
+<h3><a href="#">Hotline:016xxxxxxx</a></h3>
---
+++ Online-Tutor/inc/footer.inc.php
@@ -0,0 +1,30 @@
+
+<script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+	<div class="footerMain">
+		<div class="content">
+			<div class="footer-grids">
+				<div class="footer one">
+					<h3>More About Company</h3>
+					<p>My first Platform.</p>
+					<!-- <p class="adam">- Neehad, CEO</p>
+					<div class="clear"></div> -->
+				</div>
+				<div class="footer two">
+					<h3>Keep Connected</h3>
+					<ul>
+						<li><a class="fas fa-code" href="https://github.com/neehad1"><i></i>github</a></li>
+				
+					</ul>
+				</div>
+				<div class="footer three">
+					<h3>Contact Information</h3>
+					<ul>
+						<li>contact@example.com</li>
+						<!-- <li>0123456789</li> -->
+						
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
